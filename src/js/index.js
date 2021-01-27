c6010f93a72068891c3bffb1207748d277cdf703
Natalia-BP <nataliabarqueroperez@gmail.com>
2021-01-27 23:49:19 +0000

    ejercicio terminado de profile card

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import "../style/index.scss";
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
  * 
-    {
+    function {
         includeCover: true, // if includeCover is true the algorithm should
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
@@ -29,23 +29,72 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
+  // Inputs de texto
+  let nombre = variables.name;
+  if (variables.name == null) {
+    nombre = " ";
+  }
+
+  let apellido = variables.lastname;
+  if (variables.lastname == null) {
+    apellido = " ";
+  }
+
+  let ciudad = variables.city;
+  if (variables.city == null) {
+    ciudad = " ";
+  }
+
+  let pais = variables.country;
+  if (variables.country == null) {
+    pais = " ";
+  }
+
+  let trabajo = variables.role;
+  if (variables.role == null) {
+    trabajo = " ";
+  }
+
+  let rdTwitter = variables.twitter;
+  if (variables.twitter == null) {
+    rdTwitter = " ";
+  }
+
+  let rdGitHub = variables.github;
+  if (variables.github == null) {
+    rdGitHub = " ";
+  }
+
+  let rdLinkedin = variables.linkedin;
+  if (variables.linkedin == null) {
+    rdLinkedin = " ";
+  }
+
+  let rdInstagram = variables.instagram;
+  if (variables.instagram == null) {
+    rdInstagram = " ";
+  }
+
+  //   Boton redes
+  let social = "";
+  if (variables.socialMediaPosition != null) {
+    social = `<ul class="${variables.socialMediaPosition}">
+            <li><a href=${rdTwitter}><i class="fa fa-twitter"></i></a></li>
+            <li><a href=${rdGitHub}><i class="fa fa-github"></i></a></li>
+            <li><a href=${rdLinkedin}><i class="fa fa-linkedin"></i></a></li>
+            <li><a href=${rdInstagram}><i class="fa fa-instagram"></i></a></li>
+          </ul>`;
+  }
+
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+          <img src="${variables.avatarURL}" class="photo"/>
+          <h1>${nombre} ${apellido}</h1>
+          <h2>${trabajo}</h2>
+           <h3>${ciudad}, ${pais}</h3>
+            ${social}
+          </div>`;
 }
-
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
@@ -58,7 +107,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: null,
     // social media usernames
     twitter: null,
     github: "alesanchezr",
